9d336e6e2f6581911bc55ad4be476c905c29603b
regisf <regisfloret@protonmail.com>
2020-09-24 13:39:37 +0200

    Licence header

--- ETransactions/Devises.php
+++ ETransactions/Devises.php
@@ -1,6 +1,24 @@
 <?php
-
 /*
+ * ETransactions - Wrapper for e-transactions.fr service.
+ * Copyright (C) 2020 Regis FLORET
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
  * The Devises class allow you to set the devise you want regarding ISO
  * as long it is euro devise
  */
--- ETransactions/Values/HMACValue.php
+++ ETransactions/Values/HMACValue.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * ETransactions - Wrapper for e-transactions.fr service.
+ * Copyright (C) 2020 Regis FLORET
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 require_once 'ValueBase.php';
 
@@ -11,14 +29,14 @@ class HMACValue extends ValueBase
     /**
      * Compute the HMAC value at the object creation
      *
-     * @param $secretKey
+     * @param SecretValue $secretKey
      * @param string $parameters
      * @param HashValue $hashValue
      * @throws ValueException
      */
-    public function __construct($secretKey, $parameters, $hashValue)
+    public function __construct($secretValue, $parameters, $hashValue)
     {
-        $binKey = pack('H*', $secretKey);
+        $binKey = pack('H*', $secretValue->getValue());
         $hash = strtoupper(hash_hmac($hashValue->getValue(), $parameters, $binKey));
 
         parent::__construct($hash);
